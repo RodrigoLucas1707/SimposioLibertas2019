cf5aa9cd2b25f3d92937612e4c455d3675b659fe
potebr <igorhscarvalho@gmail.com>
2019-10-07 22:16:49 -0300

    to chegando alex

--- simposio/templates/paginacadastro.page/script_cadastrp.js
+++ simposio/templates/paginacadastro.page/script_cadastrp.js
@@ -1,17 +1,20 @@
 $( document ).ready (function(){
-    $.ajax({
-        type: 'POST',
-        data:{
-            Nome: $("#nome_id").val(),
-            Email: $("#email_id").val(),
-            Cpf: $("#cpf_id").val(),
-            usuario: $("#usuario_id").val(),
-            senha: $("#senha_id").val()
-        },
-        url: 'http://127.0.0.1:8000/cadastro',
-        sucess: function(res){
-            alert(res.token);
 
-        }
+    $("#cadastro").click(function() {
+        $.ajax({
+            type: 'POST',
+            data:{
+                Nome: $("#nome_id").val(),
+                Email: $("#email_id").val(),
+                Cpf: $("#cpf_id").val(),
+                usuario: $("#usuario_id").val(),
+                senha: $("#senha_id").val()
+            },
+            url: 'http://127.0.0.1:8000/cadastro',
+            sucess: function(res){
+                alert(res.token);
+    
+            }
+        });
     });
 }
