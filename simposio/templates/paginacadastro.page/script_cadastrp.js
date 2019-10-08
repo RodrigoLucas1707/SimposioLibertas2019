17909dacbcc7fe8a6850097a31b1b149a5923e14
potebr <igorhscarvalho@gmail.com>
2019-10-07 22:09:51 -0300

    agora vai

--- simposio/templates/paginacadastro.page/script_cadastrp.js
+++ simposio/templates/paginacadastro.page/script_cadastrp.js
@@ -2,16 +2,16 @@ $( document ).ready (function(){
     $.ajax({
         type: 'POST',
         data:{
-            Nome: "nome_id",
-            Email: "email_id",
-            Cpf: "cpf_id",
-            usuario: "usuario_id",
-            senha: "senha_id"
+            Nome: $("#nome_id").val(),
+            Email: $("#email_id").val(),
+            Cpf: $("#cpf_id").val(),
+            usuario: $("#usuario_id").val(),
+            senha: $("#senha_id").val()
         },
         url: 'http://127.0.0.1:8000/cadastro',
         sucess: function(res){
             alert(res.token);
-            
+
         }
     });
 }
