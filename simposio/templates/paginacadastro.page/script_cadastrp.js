531af9e698acc414fb2232e30f32e27ffdac0183
potebr <igorhscarvalho@gmail.com>
2019-10-07 21:55:41 -0300

    foi

---
+++ simposio/templates/paginacadastro.page/script_cadastrp.js
@@ -0,0 +1,16 @@
+$( document ).ready (function(){
+    $.ajax({
+        type: 'POST',
+        data:{
+            Nome: "nome_id",
+            Email: "email_id",
+            Cpf: "cpf_id",
+            usuario: "usuario_id",
+            senha: "senha_id"
+        },
+        url: 'http://127.0.0.1:8000/cadastro',
+        sucess: function(res){
+            alert(res.token);
+        }
+    });
+}
